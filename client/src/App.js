6dfe2e2e16173122e5db1dd0884c408185d9d649
karimi <karimidorcas1@gmail.com>
2024-05-13 08:24:15 +0300

    ft-register complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
 
 const Homepage = () => <h1>Homepage</h1>;
 const BrowseCharities = () => <h1>Browse Charities</h1>;
 const HowItWorks = () => <h1>How It Works</h1>;
-const CreateAccount = () => <h1>Create Account</h1>;
 const SignIn = () => <h1>Sign In</h1>;
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/charities" element={<BrowseCharities />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
-        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/create-account" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </Router>
