9a2256238138b923b446625c14364a4ec0d95bdf
+collins <+collinskipngetich60@gmail.com>
2024-05-15 10:25:38 +0300

    modifyed

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
-import AboutUs from './components/AboutUs.js';
+import AboutUs from './components/AboutUs';
+
 
 const Homepage = () => <h1>Homepage</h1>;
 const BrowseCharities = () => <h1>Browse Charities</h1>;
 const HowItWorks = () => <h1>How It Works</h1>;
 const SignIn = () => <h1>Sign In</h1>;
+
 
 const App = () => {
   return (
