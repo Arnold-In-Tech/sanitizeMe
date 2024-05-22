d37c0bcaac4b5d30a8cba39672ba1c6ff7607897
bradleymbuvi <bradwent@gmail.com>
2024-05-22 16:28:53 +0300

    Resize background picture, handleNavigate for Donate Now button

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Partners from './Partners';
 import HumanitarianMission from './HumanitarianMission';
 import '../stylesheets/Homepage.css';
+import { useNavigate} from "react-router-dom";
 
-const Homepage = () => (
+const Homepage = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () =>{ 
+    navigate("/charities");
+  }
+  return(
   <div className="homepage">
     <div className="message-container">
       <h1 className="main-message">Happiness comes from your action.</h1>
       <p className="subtitle">Be part of the breakthrough and make someone's dream come true.</p>
       <div className="button-container">
-        <button className="action-button">Donate Now</button>
+        <button onClick={handleNavigate} className="action-button">Donate Now</button>
         <button className="action-button">Watch Video</button>
       </div>
     </div>
@@ -19,5 +25,6 @@ const Homepage = () => (
     </div>
   </div>
 );
+}
 
 export default Homepage;
