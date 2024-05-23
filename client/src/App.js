d0f321005c741ce59896a738fe4d9c286154d89f
+collins <+collinskipngetich60@gmail.com>
2024-05-23 09:34:25 +0300

    done

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,14 @@ import Footer from './components/Footer';
 import OpenDonations from './components/OpenDonations';
 import CharityDetails from './components/CharityDetails';
 import Donations from './components/Donations';
-import AboutUs from './components/AboutUs';
 import Homepage from './components/Homepage';
 import RegisterCharity from './components/Register_charity';
 import MyCharities from './components/My_Charities'
+import AboutUs from './components/AboutUs';
 
 
 
 const HowItWorks = () => <h1>How It Works</h1>;
-const MyCharities = () => <h1>My charities</h1>;
-const AboutUs = () => <h1>About Us</h1>;
 
 const App = () => {
   return (
